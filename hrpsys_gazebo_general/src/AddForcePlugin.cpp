7f7c64be33eabc05d313b6e2f5e8e8de6d7a1938
murooka@jsk.imi.i.u-tokyo.ac.jp <murooka@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-08-25 10:04:32 +0000

    enable to set force and torque with ROS in AddForcePlugin

--- hrpsys_gazebo_general/src/AddForcePlugin.cpp
+++ hrpsys_gazebo_general/src/AddForcePlugin.cpp
@@ -1,9 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
 #include <boost/bind.hpp>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
-#include <iostream>
-#include <stdio.h>
+
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <ros/advertise_options.h>
+#include <ros/subscribe_options.h>
+
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Wrench.h>
+#include <std_msgs/String.h>
+
 
 namespace gazebo
 {
@@ -11,6 +24,7 @@ namespace gazebo
   {
 
   public:
+    // Initialize
     void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
     {
       // Store the pointer to the model
@@ -29,6 +43,10 @@ namespace gazebo
       if (_sdf->HasElement("torque")) {
 	this->torque = _sdf->Get<math::Vector3>("torque");
       }
+      this->position = math::Vector3(0, 0, 0);
+      if(_sdf->HasElement("position")) {
+	this->position = _sdf->Get<math::Vector3>("position");
+      }
 
       // find root link
       this->link = this->model->GetLink(this->link_name);
@@ -37,17 +55,77 @@ namespace gazebo
 	return;
       }
       
+      // Make sure the ROS node for Gazebo has already been initialized                                                                                    
+      if (!ros::isInitialized()) {
+	gzerr << "A ROS node for Gazebo has not been initialized, unable to load plugin. "
+	      << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)";
+	return;
+      }
+      // ros node
+      this->rosNode = new ros::NodeHandle("");
+      // ros callback queue for processing subscription
+      this->deferredLoadThread = boost::thread(boost::bind(&AddForce::DeferredLoad, this));
+
+      gzmsg << "AddForcePlugin was loaded ! ( force: " << this->force << "  torque: " << this->torque << "  position: " << this->position << ")" << std::endl;
+    }
+
+    void DeferredLoad() {
+      // ros topic subscribtions
+      ros::SubscribeOptions ForceCommandSo =
+	ros::SubscribeOptions::create<geometry_msgs::Wrench>("/AddForcePlugin/ForceCommand", 100,
+						    boost::bind(&AddForce::SetForceCommand, this, _1),
+						    ros::VoidPtr(), &this->rosQueue);
+      ros::SubscribeOptions ForcePositionSo =
+	ros::SubscribeOptions::create<geometry_msgs::Vector3>("/AddForcePlugin/ForcePosition", 100,
+					      boost::bind(&AddForce::SetForcePosition, this, _1),
+					      ros::VoidPtr(), &this->rosQueue);
+      // Enable TCP_NODELAY because TCP causes bursty communication with high jitter,
+      ForceCommandSo.transport_hints = ros::TransportHints().reliable().tcpNoDelay(true);
+      this->subForceCommand = this->rosNode->subscribe(ForceCommandSo);
+      ForcePositionSo.transport_hints = ros::TransportHints().reliable().tcpNoDelay(true);
+      this->subForcePosition = this->rosNode->subscribe(ForcePositionSo);
+
+      // ros callback queue for processing subscription
+      this->callbackQueeuThread = boost::thread(boost::bind(&AddForce::RosQueueThread, this));
+
       // Listen to the update event.
       this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&AddForce::OnUpdate, this, _1));
+    }
 
-      gzmsg << "AddForcePlugin was loaded ! ( force: " << this->force << "  torque: " << this->torque << " )" << std::endl;
+    void SetForceCommand(const geometry_msgs::Wrench::ConstPtr &_msg)
+    {
+      this->force.x = _msg->force.x;
+      this->force.y = _msg->force.y;
+      this->force.z = _msg->force.z;
+      this->torque.x = _msg->torque.x;
+      this->torque.y = _msg->torque.y;
+      this->torque.z = _msg->torque.z;
+      gzmsg << "subscribed AddForceCommand. ( force: " << this->force << "  torque: " << this->torque << " )" << std::endl;
+    }
+
+    void SetForcePosition(const geometry_msgs::Vector3::ConstPtr &_msg)
+    {
+      this->position.x = _msg->x;
+      this->position.y = _msg->y;
+      this->position.z = _msg->z;
+      gzmsg << "subscribed AddForcePosition. ( position: " << this->position << " )" << std::endl;
     }
 
     // Called by the world update start event
     void OnUpdate(const common::UpdateInfo & /*_info*/)
     {
-      this->link->AddForce(this->force);
+      this->link->AddRelativeForce(this->force);
+      // this->link->AddForceAtRelativePosition(this->force, this->position);
       this->link->AddTorque(this->torque);
+    }
+
+    // Ros loop thread function
+    void RosQueueThread() {
+      static const double timeout = 0.01;
+
+      while (this->rosNode->ok()) {
+	this->rosQueue.callAvailable(ros::WallDuration(timeout));
+      }
     }
 
   private:
@@ -55,9 +133,16 @@ namespace gazebo
     std::string link_name;
     math::Vector3 force;
     math::Vector3 torque;
+    math::Vector3 position;
     physics::LinkPtr link;
     event::ConnectionPtr updateConnection;
 
+    ros::NodeHandle* rosNode;
+    ros::CallbackQueue rosQueue;
+    ros::Subscriber subForceCommand;
+    ros::Subscriber subForcePosition;
+    boost::thread callbackQueeuThread;
+    boost::thread deferredLoadThread;
   };
 
   GZ_REGISTER_MODEL_PLUGIN(AddForce)
