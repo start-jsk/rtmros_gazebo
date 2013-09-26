6aaa0306ac23f956d697456e5cd9f1228bbfb4e2
youhei@jsk.imi.i.u-tokyo.ac.jp <youhei@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-09-26 16:55:30 +0000

    modify for setting robot model directory

--- hrpsys_gazebo_tutorials/robot_models/HRP2JSKNTS/install_robot.sh
+++ hrpsys_gazebo_tutorials/robot_models/HRP2JSKNTS/install_robot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$(rospack find hrpsys_gazebo_tutorials)/robot_models/install_robot_common.sh HRP2JSKNTS
+$(rospack find hrpsys_gazebo_tutorials)/robot_models/install_robot_common.sh HRP2JSKNTS $(rospack find jsk_hrpsys_ros_bridge)/models
--- hrpsys_gazebo_tutorials/robot_models/install_robot_common.sh
+++ hrpsys_gazebo_tutorials/robot_models/install_robot_common.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # usage
-# ./install_robot.sh ROBOT_NAME
+# ./install_robot.sh ROBOT_NAME (model directory)
 
 ROBOT_NAME=$1
+if [ $# -gt 1 ]; then
+    INPUT_DIR=$2
+else
+    INPUT_DIR=$(rospack find hrpsys_ros_bridge_tutorials)/models
+fi
+
 OUTPUT_DIR=$(rospack find hrpsys_gazebo_tutorials)/robot_models/${ROBOT_NAME}
 OUTPUT_FILE=${OUTPUT_DIR}/${ROBOT_NAME}.urdf
 SED_SCRIPT_FILE=${OUTPUT_DIR}/${ROBOT_NAME}_optional_urdf_setting.sh
@@ -11,15 +17,15 @@ SED_SCRIPT_FILE=${OUTPUT_DIR}/${ROBOT_NAME}_optional_urdf_setting.sh
 if [ ! -e ${OUTPUT_FILE} ]; then
 ##
     mkdir -p ${OUTPUT_DIR}/meshes
-    rosrun collada_tools collada_to_urdf $(rospack find hrpsys_ros_bridge_tutorials)/models/${ROBOT_NAME}.dae -G -A --mesh_output_dir ${OUTPUT_DIR}/meshes --mesh_prefix "package://${ROBOT_NAME}/meshes" --output_file=${OUTPUT_FILE}
+    rosrun collada_tools collada_to_urdf ${INPUT_DIR}/${ROBOT_NAME}.dae -G -A --mesh_output_dir ${OUTPUT_DIR}/meshes --mesh_prefix "package://${ROBOT_NAME}/meshes" --output_file=${OUTPUT_FILE}
 ## execute sed
     ${SED_SCRIPT_FILE} ${OUTPUT_FILE}
 fi
 
 if [ ! -e ${OUTPUT_DIR}/hrpsys ]; then
     mkdir -p ${OUTPUT_DIR}/hrpsys
-    ln -s  $(rospack find hrpsys_ros_bridge_tutorials)/models/${ROBOT_NAME}.conf ${OUTPUT_DIR}/hrpsys
-    ln -s  $(rospack find hrpsys_ros_bridge_tutorials)/models/${ROBOT_NAME}.RobotHardware.conf ${OUTPUT_DIR}/hrpsys
-    ln -s  $(rospack find hrpsys_ros_bridge_tutorials)/models/${ROBOT_NAME}.dae ${OUTPUT_DIR}/hrpsys
-    ln -s  $(rospack find hrpsys_ros_bridge_tutorials)/models/${ROBOT_NAME}_nosim.xml ${OUTPUT_DIR}/hrpsys
+    ln -s  ${INPUT_DIR}/${ROBOT_NAME}.conf ${OUTPUT_DIR}/hrpsys
+    ln -s  ${INPUT_DIR}/${ROBOT_NAME}.RobotHardware.conf ${OUTPUT_DIR}/hrpsys
+    ln -s  ${INPUT_DIR}/${ROBOT_NAME}.dae ${OUTPUT_DIR}/hrpsys
+    ln -s  ${INPUT_DIR}/${ROBOT_NAME}_nosim.xml ${OUTPUT_DIR}/hrpsys
 fi
