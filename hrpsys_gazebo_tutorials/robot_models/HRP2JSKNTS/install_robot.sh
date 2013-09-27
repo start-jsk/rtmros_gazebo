3873b517bc6c4cc580687b812d9a0bbe74c0f1ed
notheworld@gmail.com <notheworld@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-09-27 05:08:19 +0000

    remove optional argument for install_robot_common.sh ;; HRP2JSK.dae and HRP2JSKNTS.dae are generated in hrpsys_ros_bridge_tutorials/models directory after r5619

--- hrpsys_gazebo_tutorials/robot_models/HRP2JSKNTS/install_robot.sh
+++ hrpsys_gazebo_tutorials/robot_models/HRP2JSKNTS/install_robot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$(rospack find hrpsys_gazebo_tutorials)/robot_models/install_robot_common.sh HRP2JSKNTS $(rospack find jsk_hrpsys_ros_bridge)/models
+$(rospack find hrpsys_gazebo_tutorials)/robot_models/install_robot_common.sh HRP2JSKNTS
