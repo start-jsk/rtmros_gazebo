4d17972094abb77c90462c487e819421a3b1637c
garaemon@gmail.com <garaemon@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-11-17 16:48:00 +0000

    adding circle to image_view2

--- hrpsys_gazebo_atlas/scripts/triangle_screenpoint.py
+++ hrpsys_gazebo_atlas/scripts/triangle_screenpoint.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import math
+
 import roslib
 import rospy
 
@@ -7,7 +9,8 @@ roslib.load_manifest("hrpsys_gazebo_atlas")
 
 from visualization_msgs.msg import Marker
 from image_view2.msg import ImageMarker2
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PointStamped, Point
+from std_msgs.msg import ColorRGBA
 from hrpsys_gazebo_atlas.msg import TrianglePoints
 from std_srvs.srv import Empty, EmptyResponse
 # global variables
@@ -21,6 +24,7 @@ def publishMode():
   global g_image_marker_pub
   marker = ImageMarker2()
   marker.header.stamp = rospy.Time(0.0)
+  marker.id = 1
   marker.type = ImageMarker2.TEXT
   marker.text = "mode: triangle_screenpoint.py"
   marker.position.x = 300
@@ -30,14 +34,36 @@ def publishMode():
 class TrianglePointsState():
   def __init__(self):
     self.reset()
-    self.triangle_publisher = rospy.Publisher("/trianglepoints", TrianglePoints)
-    self.point_a_publisher = rospy.Publisher("/triangle_point_a", PointStamped)
-    self.point_b_publisher = rospy.Publisher("/triangle_point_b", PointStamped)
-    self.point_c_publisher = rospy.Publisher("/triangle_point_c", PointStamped)
+    self.triangle_publisher = rospy.Publisher("/trianglepoints", 
+                                              TrianglePoints)
+    self.point_a_publisher = rospy.Publisher("/triangle_point_a",
+                                             PointStamped)
+    self.point_b_publisher = rospy.Publisher("/triangle_point_b", 
+                                             PointStamped)
+    self.point_c_publisher = rospy.Publisher("/triangle_point_c", 
+                                             PointStamped)
   def reset(self):
     self.a = None
     self.b = None
     self.c = None
+  def makeImageMarker(self, p):
+    now = rospy.Time(0.0)
+    marker = ImageMarker2()
+    marker.header.stamp = now
+    marker.header.frame_id = p.header.frame_id
+    marker.type = ImageMarker2.POLYGON3D
+    marker.points3D.header.frame_id = p.header.frame_id
+    marker.points3D.header.stamp = now
+    R = 0.01
+    N = 10
+    for i in range(N) + [0]:
+      point = Point()
+      point.x = p.point.x + R * math.cos(2 * math.pi * i / N)
+      point.y = p.point.y + R * math.sin(2 * math.pi * i / N)
+      point.z = p.point.z
+      marker.points3D.points.append(point)
+    marker.outline_colors = [ColorRGBA(1.0, .0, .0, 1.0)]
+    return marker
   def publishTriangles(self):
     msg = TrianglePoints()
     msg.header.stamp = rospy.Time()
@@ -46,12 +72,26 @@ class TrianglePointsState():
     msg.c = self.c
     self.triangle_publisher.publish(msg)
   def publishHelperTopics(self):
+    global g_image_marker_pub
     if self.a:
       self.point_a_publisher.publish(self.a)
+      m = self.makeImageMarker(self.a)
+      m.id = 2
+      g_image_marker_pub.publish(m)
     if self.b:
       self.point_b_publisher.publish(self.b)
+      m = self.makeImageMarker(self.b)
+      m.id = 3
+      m.outline_colors[0].r = 0.0
+      m.outline_colors[0].g = 1.0
+      g_image_marker_pub.publish(m)
     if self.c:
       self.point_c_publisher.publish(self.c)
+      m = self.makeImageMarker(self.c)
+      m.outline_colors[0].r = 0.0
+      m.outline_colors[0].b = 1.0
+      m.id = 4
+      g_image_marker_pub.publish(m)
     publishMode()
 
 def pointCB(msg):
