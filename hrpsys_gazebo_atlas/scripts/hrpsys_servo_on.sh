8c47dc8135e8f44e8a1123055ea365182d971132
furuta@jsk.imi.i.u-tokyo.ac.jp <furuta@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-12-04 08:02:42 +0000

    fix path to resetJointGroup.py

--- hrpsys_gazebo_atlas/scripts/hrpsys_servo_on.sh
+++ hrpsys_gazebo_atlas/scripts/hrpsys_servo_on.sh
@@ -2,7 +2,7 @@
 
 rosservice call /RobotHardwareServiceROSBridge/power '{name: "all", ss: 0}'
 sleep 1
-./resetJointGroup.py RobotHardware0 $(rospack find hrpsys_gazebo_atlas)/models/atlas_v3.dae -ORBInitRef NameService=corbaloc:iiop:localhost:15005/NameService
+$(rospack find hrpsys_gazebo_atlas)/scripts/resetJointGroup.py RobotHardware0 $(rospack find hrpsys_gazebo_atlas)/models/atlas_v3.dae -ORBInitRef NameService=corbaloc:iiop:localhost:15005/NameService
 sleep 1
 rosservice call /StateHolderServiceROSBridge/goActual
 sleep 1
