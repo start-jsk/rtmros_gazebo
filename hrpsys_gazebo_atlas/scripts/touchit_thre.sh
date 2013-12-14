6acdff0ba3aaa91076a9b5591e3dcfed5e551344
murooka@jsk.imi.i.u-tokyo.ac.jp <murooka@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2013-12-14 08:10:59 +0000

    deleted debug lines and enable to set negative value for argument.

--- hrpsys_gazebo_atlas/scripts/touchit_thre.sh
+++ hrpsys_gazebo_atlas/scripts/touchit_thre.sh
@@ -1,13 +1,11 @@
 #!/bin/bash 
 
 thre=20
-echo $#
-echo $1
 if [ $# -eq 1 ]
 then
 thre=$1
 fi
 
 echo -e "\e[1;31mset touchit/thre $thre\e[m"
-rostopic pub /touchit/thre std_msgs/Float64 $thre -1
+rostopic pub -1 /touchit/thre std_msgs/Float64 -- $thre
 
