05fdeffd8cfd1752db2d3eaa44f457bb95c422b0
youhei@jsk.imi.i.u-tokyo.ac.jp <youhei@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-01-10 08:53:07 +0000

    add setting for drcsim

--- hrpsys_gazebo_tutorials/setup.sh
+++ hrpsys_gazebo_tutorials/setup.sh
@@ -1,3 +1,24 @@
+if [ ! -e /usr/share/drcsim/setup.sh ]; then
+    echo -e "\e[31mdrcsim should be installed for using hrpsys_gazebo\e[m"
+    echo -e "\e[31msee http://gazebosim.org/wiki/DRC/Install\e[m"
+    exit 1
+fi
+
+ROS_PACKAGE_PATH_ORG=$ROS_PACKAGE_PATH
+
+unset GAZEBO_MODEL_PATH
+unset GAZEBO_RESOURCE_PATH
+unset GAZEBO_MASTER_URI
+unset GAZEBO_PLUGIN_PATH
+unset GAZEBO_MODEL_DATABASE_URI
+source /usr/share/drcsim/setup.sh
+
+## append original package path because /opt/ros/groovy/setup.sh have been called in drcsim/setup.sh
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH_ORG:$ROS_PACKAGE_PATH
+## sort package path
+export ROS_PACKAGE_PATH=`echo $(echo $ROS_PACKAGE_PATH | sed -e "s/:/\n/g" | awk '!($0 in A) && A[$0] = 1' | grep -v "opt/ros"; echo $ROS_PACKAGE_PATH | sed -e "s/:/\n/g" | awk '!($0 in A) && A[$0] = 1' | grep "opt/ros") | sed -e "s/ /:/g"`
+
+##
 pkgdir=`rospack find hrpsys_gazebo_general`
 tpkgdir=`rospack find hrpsys_gazebo_tutorials`
 drcdir=`rospack find drcsim_model_resources`
