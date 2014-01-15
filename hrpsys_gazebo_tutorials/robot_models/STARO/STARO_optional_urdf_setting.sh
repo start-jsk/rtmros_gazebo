a9fa50c38fb4d5528f287b10daadeed8c1c4484c
murooka@jsk.imi.i.u-tokyo.ac.jp <murooka@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-01-15 06:30:18 +0000

    replace fixed to revolute for STARO HEAD_JOINT0

--- hrpsys_gazebo_tutorials/robot_models/STARO/STARO_optional_urdf_setting.sh
+++ hrpsys_gazebo_tutorials/robot_models/STARO/STARO_optional_urdf_setting.sh
@@ -9,4 +9,5 @@ sed -i -e 's@</robot>@  <gazebo>\n    <plugin filename="libIOBPlugin.so" name="h
 sed -i -e 's@</robot>@  <gazebo reference="CHEST_LINK1" >\n    <sensor name="waist_imu" type="imu">\n      <always_on>1</always_on>\n      <update_rate>1000.0</update_rate>\n      <imu>\n        <noise>\n          <type>gaussian</type>\n          <rate>\n            <mean>0.0</mean>\n            <stddev>2e-4</stddev>\n            <bias_mean>0.0000075</bias_mean>\n            <bias_stddev>0.0000008</bias_stddev>\n          </rate>\n          <accel>\n            <mean>0.0</mean>\n            <stddev>1.7e-2</stddev>\n            <bias_mean>0.1</bias_mean>\n            <bias_stddev>0.001</bias_stddev>\n          </accel>\n        </noise>\n      </imu>\n    </sensor>\n  </gazebo>\n</robot>@g' ${OUTPUT_FILE}
 # continuous joint not working in GAZEBO
 sed -i -e 's@continuous@revolute@g' ${OUTPUT_FILE}
+sed -i -e 's@<joint name="HEAD_JOINT0" type="fixed">@<joint name="HEAD_JOINT0" type="revolute">@g' ${OUTPUT_FILE}
 
