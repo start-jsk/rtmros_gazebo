89e6a92e296ba626d698fdcad6d7cacd161ba194
murooka@jsk.imi.i.u-tokyo.ac.jp <murooka@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-01-22 03:43:32 +0000

    use L/R_ARM_JOINT7 for HRP2JSKNT_with_HRP3HAND because hrpsys model has that joint.

--- hrpsys_gazebo_tutorials/robot_models/HRP2JSKNT/HRP2JSKNT_optional_urdf_setting.sh
+++ hrpsys_gazebo_tutorials/robot_models/HRP2JSKNT/HRP2JSKNT_optional_urdf_setting.sh
@@ -8,3 +8,7 @@ sed -i -e 's@</robot>@  <gazebo>\n    <plugin filename="libIOBPlugin.so" name="h
 # continuous joint not working in GAZEBO
 sed -i -e 's@continuous@revolute@g' ${OUTPUT_FILE}
 
+# overwrite mass and inertia which have invalid settings.
+sed -i -e 's@<mass value="0" />@<mass value="1" />@g' ${OUTPUT_FILE}
+sed -i -e 's@<inertia ixx="0" ixy="0" ixz="0" iyy="0" iyz="0" izz="0"/>@<inertia ixx="1" ixy="0" ixz="0" iyy="1" iyz="0" izz="1"/>@g' ${OUTPUT_FILE}
+
--- hrpsys_gazebo_tutorials/robot_models/HRP2JSKNT_with_HRP3HAND/generate_HRP2JSKNT_with_HRP3HAND.bash
+++ hrpsys_gazebo_tutorials/robot_models/HRP2JSKNT_with_HRP3HAND/generate_HRP2JSKNT_with_HRP3HAND.bash
@@ -12,17 +12,17 @@ L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "
 L_END=`expr ${L_START} + ${L_END} - 1`
 sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
 
-# remove LARM_LINK7
-L_START=`grep -n "<link name=\"LARM_LINK7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
-L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
-L_END=`expr ${L_START} + ${L_END} - 1`
-sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
+# # remove LARM_LINK7
+# L_START=`grep -n "<link name=\"LARM_LINK7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
+# L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
+# L_END=`expr ${L_START} + ${L_END} - 1`
+# sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
 
-# remove LARM_JOINT7
-L_START=`grep -n "<joint name=\"LARM_JOINT7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
-L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
-L_END=`expr ${L_START} + ${L_END} - 1`
-sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
+# # remove LARM_JOINT7
+# L_START=`grep -n "<joint name=\"LARM_JOINT7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
+# L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
+# L_END=`expr ${L_START} + ${L_END} - 1`
+# sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
 
 # remove RARM_LINK6
 L_START=`grep -n "<link name=\"RARM_LINK6\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
@@ -30,17 +30,17 @@ L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "
 L_END=`expr ${L_START} + ${L_END} - 1`
 sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
 
-# remove RARM_LINK7
-L_START=`grep -n "<link name=\"RARM_LINK7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
-L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
-L_END=`expr ${L_START} + ${L_END} - 1`
-sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
+# # remove RARM_LINK7
+# L_START=`grep -n "<link name=\"RARM_LINK7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
+# L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
+# L_END=`expr ${L_START} + ${L_END} - 1`
+# sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
 
-# remove RARM_JOINT7
-L_START=`grep -n "<joint name=\"RARM_JOINT7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
-L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
-L_END=`expr ${L_START} + ${L_END} - 1`
-sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
+# # remove RARM_JOINT7
+# L_START=`grep -n "<joint name=\"RARM_JOINT7\"" HRP2JSKNT_modified_for_hrp3hand.urdf -m 1 | cut -f1 -d:`
+# L_END=$(sed -n "${L_START},\$p" HRP2JSKNT_modified_for_hrp3hand.urdf | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
+# L_END=`expr ${L_START} + ${L_END} - 1`
+# sed -i -e "${L_START},${L_END}d" HRP2JSKNT_modified_for_hrp3hand.urdf
 
 # generate URDF with xacro
 rosrun xacro xacro.py HRP2JSKNT_with_HRP3HAND.urdf.xacro > HRP2JSKNT_with_HRP3HAND.urdf
