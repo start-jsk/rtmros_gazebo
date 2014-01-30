5a06a7a4301853ab52468b06817b7d13feb3c0da
murooka@jsk.imi.i.u-tokyo.ac.jp <murooka@jsk.imi.i.u-tokyo.ac.jp@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-01-30 17:50:51 +0000

    delete HRP2JSKNT_with_HRP3HAND directory and related files, and generate HRP2JSKNT.urdf which has HRP3HAND and Kinect.

--- hrpsys_gazebo_tutorials/robot_models/HRP2JSKNT/HRP2JSKNT_optional_urdf_setting.sh
+++ hrpsys_gazebo_tutorials/robot_models/HRP2JSKNT/HRP2JSKNT_optional_urdf_setting.sh
@@ -9,8 +9,8 @@ sed -i -e 's@</robot>@  <gazebo>\n    <plugin filename="libIOBPlugin.so" name="h
 sed -i -e 's@continuous@revolute@g' ${OUTPUT_FILE}
 
 # overwrite mass and inertia which have invalid settings.
-sed -i -e 's@<mass value="0" />@<mass value="1" />@g' ${OUTPUT_FILE}
-sed -i -e 's@<inertia ixx="0" ixy="0" ixz="0" iyy="0" iyz="0" izz="0"/>@<inertia ixx="1" ixy="0" ixz="0" iyy="1" iyz="0" izz="1"/>@g' ${OUTPUT_FILE}
+sed -i -e 's@<mass value="0" />@<mass value="0.1" />@g' ${OUTPUT_FILE}
+sed -i -e 's@<inertia ixx="0" ixy="0" ixz="0" iyy="0" iyz="0" izz="0"/>@<inertia ixx="0.01" ixy="0" ixz="0" iyy="0.01" iyz="0" izz="0.01"/>@g' ${OUTPUT_FILE}
 
 ## change foot parameters
 sed -i -e '/<gazebo reference="LLEG_LINK5">/{N;N;N;N;s@  <gazebo reference="LLEG_LINK5">\n    <mu1>0.9</mu1>\n    <mu2>0.9</mu2>\n  </gazebo>@  <gazebo reference="LLEG_LINK5">\n    <kp>1000000.0</kp>\n    <kd>100.0</kd>\n    <mu1>1.5</mu1>\n    <mu2>1.5</mu2>\n    <fdir1>1 0 0</fdir1>\n    <maxVel>10.0</maxVel>\n    <minDepth>0.00</minDepth>\n  </gazebo>@;}' ${OUTPUT_FILE}
@@ -24,9 +24,28 @@ sed -i -e '/<collision>/{N;N;N;s@<collision>\n      <origin xyz="0 0 0" rpy="0 -
 sed -i -e '/<collision>/{N;N;N;N;N;s@<collision>\n      <origin xyz="0.115 0 -0.074" rpy="0 -0 0"/>\n      <geometry>\n        <mesh filename="package://HRP2JSKNT/meshes/LLEG_LINK6_mesh.dae" scale="1 1 1" />\n      </geometry>\n    </collision>@@;}' ${OUTPUT_FILE}
 sed -i -e '/<collision>/{N;N;N;N;N;s@<collision>\n      <origin xyz="0.115 0 -0.074" rpy="0 -0 0"/>\n      <geometry>\n        <mesh filename="package://HRP2JSKNT/meshes/RLEG_LINK6_mesh.dae" scale="1 1 1" />\n      </geometry>\n    </collision>@@;}' ${OUTPUT_FILE}
 
+# remove LARM_LINK6
+L_START=`grep -n "<link name=\"LARM_LINK6\"" -m 1 ${OUTPUT_FILE} -m 1 | cut -f1 -d:`
+L_END=$(sed -n "${L_START},\$p" ${OUTPUT_FILE} | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
+L_END=`expr ${L_START} + ${L_END} - 1`
+sed -i -e "${L_START},${L_END}d" ${OUTPUT_FILE}
+
+# remove RARM_LINK6
+L_START=`grep -n "<link name=\"RARM_LINK6\"" ${OUTPUT_FILE} -m 1 | cut -f1 -d:`
+L_END=$(sed -n "${L_START},\$p" ${OUTPUT_FILE} | grep -n "<\/gazebo>" -m 1 | cut -f1 -d:)
+L_END=`expr ${L_START} + ${L_END} - 1`
+sed -i -e "${L_START},${L_END}d" ${OUTPUT_FILE}
+
 # continuous joint not working in GAZEBO
 sed -i -e 's@continuous@revolute@g' ${OUTPUT_FILE}
 
 # overwrite velocity limit because collada2urdf doesn't reflect the velocity limit of collada model.
 sed -i -e 's@velocity="0.5"@velocity="10.0"@g' ${OUTPUT_FILE}
 sed -i -e 's@effort="100"@effort="1000"@g' ${OUTPUT_FILE}
+
+# generate URDF with xacro
+cp ${OUTPUT_FILE} `echo ${OUTPUT_FILE} | sed "s/.urdf/_only_body.urdf/g"`
+rosrun xacro xacro.py `echo ${OUTPUT_FILE} | sed "s/.urdf/.urdf.xacro/g"` > ${OUTPUT_FILE}
+
+# delete Kinect link visual and collision
+sed -i -e '/<visual>/{N;N;N;N;N;N;N;N;N;N;N;s@<visual>\n      <origin rpy="0 0 0" xyz="0 0 0"/>\n      <geometry>\n        <mesh filename="package://hector_sensors_description/meshes/asus_camera/asus_camera_simple.dae"/>\n      </geometry>\n    </visual>\n    <collision>\n      <origin rpy="0 0 0" xyz="0 0 0"/>\n      <geometry>\n        <box size="0.035 0.185 0.025"/>\n      </geometry>\n    </collision>@@;}' ${OUTPUT_FILE}
--- hrpsys_gazebo_tutorials/setup.sh
+++ hrpsys_gazebo_tutorials/setup.sh
@@ -22,11 +22,12 @@ export ROS_PACKAGE_PATH=`echo $(echo $ROS_PACKAGE_PATH | sed -e "s/:/\n/g" | awk
 pkgdir=`rospack find hrpsys_gazebo_general`
 tpkgdir=`rospack find hrpsys_gazebo_tutorials`
 drcdir=`rospack find drcsim_model_resources`
+hectdir=`rosstack find hector_models`
 
 if [ -e ${pkgdir} -a -e ${tpkgdir} ]; then
     for dname in `find ${tpkgdir}/robot_models -mindepth 1 -maxdepth 1 -type d -regex '.*[^\.svn]'`; do export ROS_PACKAGE_PATH=${dname}:$ROS_PACKAGE_PATH; done
 #    export ROS_PACKAGE_PATH=${pkgdir}/ros:$ROS_PACKAGE_PATH
     export GAZEBO_RESOURCE_PATH=${tpkgdir}/worlds:$GAZEBO_RESOURCE_PATH
-    export GAZEBO_MODEL_PATH=${tpkgdir}/robot_models:${tpkgdir}/environment_models:$GAZEBO_MODEL_PATH:${drcdir}/gazebo_models/environments
+    export GAZEBO_MODEL_PATH=${tpkgdir}/robot_models:${tpkgdir}/environment_models:$GAZEBO_MODEL_PATH:${drcdir}/gazebo_models/environments:${hectdir}
     export GAZEBO_PLUGIN_PATH=${pkgdir}/plugins:$GAZEBO_PLUGIN_PATH
 fi
