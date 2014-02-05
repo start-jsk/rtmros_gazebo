6394ead88faa3d936b2de2c1b76c172ebfe15e6b
m117120@gmail.com <m117120@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-02-05 10:09:56 +0000

    added parent directory of hrpsys_gazebo_tutorials to GAZEBO_MODEL_PATH in setup.sh

--- hrpsys_gazebo_tutorials/setup.sh
+++ hrpsys_gazebo_tutorials/setup.sh
@@ -45,6 +45,6 @@ if [ -e ${pkgdir} -a -e ${tpkgdir} ]; then
     for dname in `find ${tpkgdir}/robot_models -mindepth 1 -maxdepth 1 -type d -regex '.*[^\.svn]'`; do export ROS_PACKAGE_PATH=${dname}:$ROS_PACKAGE_PATH; done
 #    export ROS_PACKAGE_PATH=${pkgdir}/ros:$ROS_PACKAGE_PATH
     export GAZEBO_RESOURCE_PATH=${tpkgdir}/worlds:$GAZEBO_RESOURCE_PATH
-    export GAZEBO_MODEL_PATH=${tpkgdir}/robot_models:${tpkgdir}/environment_models:$GAZEBO_MODEL_PATH:${drcdir}/gazebo_models/environments:${hectdir}
+    export GAZEBO_MODEL_PATH=${tpkgdir}/robot_models:${tpkgdir}/environment_models:$GAZEBO_MODEL_PATH:${drcdir}/gazebo_models/environments:${hectdir}:${tpkgdir}/..
     export GAZEBO_PLUGIN_PATH=${pkgdir}/plugins:$GAZEBO_PLUGIN_PATH
 fi
