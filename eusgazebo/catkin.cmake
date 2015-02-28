037797621c8c2ea739b11184b97c814c85037672
Ryohei Ueda <garaemon@gmail.com>
2015-02-28 17:10:43 +0900

    [eusgazebo] Add roseus to dependency

--- eusgazebo/catkin.cmake
+++ eusgazebo/catkin.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(eusgazebo)
 
-find_package(catkin REQUIRED COMPONENTS message_generation gazebo_msgs rostest)
+find_package(catkin REQUIRED COMPONENTS message_generation gazebo_msgs rostest roseus)
 
-catkin_package(CATKIN_DEPENDS message_runtime gazebo_msgs)
+catkin_package(CATKIN_DEPENDS message_runtime gazebo_msgs roseus)
 
 ## Install ##
 install(DIRECTORY euslisp test scripts samples
